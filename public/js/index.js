709d851b7a6da88913079d1d32cc18dd368d4088
Patrick <p.offenberg@gmx.net>
2018-06-25 16:58:55 +0200

    io.commit for communication to all

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Andrew',
-        text: 'yup, that works for me'
-    })
 });
 
 socket.on('disconnect', function () {
