68c13a84a60351d9e7bae051c6445271abc20897
Patrick <p.offenberg@gmx.net>
2018-06-25 17:16:53 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,14 +15,32 @@ app.use(express.static(publicPath, { index: 'index.html' }))
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+    
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
 
-        io.emit('newMessage', {
+        io.emit('newmessage', {
             from: message.from,
             text: message.text,
-            createdAt: new Date().getTime()
+            createdat: new date().gettime()
         });
+
+        //socket.broadcast.emit('newMessage', {
+        //    from: message.from,
+        //    text: message.text,
+        //    createdAt: new Date().getTime()
+        //});
     });
 
     socket.on('disconnect', () => {
